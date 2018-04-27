3d93f95f5014c2f6e0c10993608eb51d63ae50ac
Jzake <32932965+Jzake@users.noreply.github.com>
2018-04-27 14:03:59 +0200

    COMMIT PHASE 1 WAOUW

---
+++ hellowolrdmaven/src/main/java/fr/exia/hellowolrdmaven/App.java
@@ -0,0 +1,13 @@
+package fr.exia.hellowolrdmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
